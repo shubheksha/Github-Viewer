56704f2c4082bc444d3dbc8bcb9013afe6ad0b4e
Shubheksha Jalan <shubheksha@outlook.com>
2016-01-20 01:01:07 +0530

    service working correctly

---
+++ controllers/MainController.js
@@ -0,0 +1,22 @@
+(function () {
+	var app = angular.module("githubViewer", []);
+	var MainController = function ($http) {
+		this.message = "Hi, Angular!";
+		this.user = "o";
+		this.err = null;
+		var onResponse = function (response) {
+			this.user = response.data;
+		};
+
+		var onError = function () {
+			this.err = "Couldn't fetch any data, sorry!";
+		};
+
+		$http.get("https://api.github.com/users/odetocode")
+			.then(onResponse.bind(this), onError.bind(this));
+
+	};
+	app.controller("MainController", MainController);
+
+
+}());
